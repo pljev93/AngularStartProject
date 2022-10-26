c6f2bf1332a2b34f7cf0c236817a73c6764999ce
Oksiaction1601 <pl160194poa@gmail.com>
2022-10-26 22:45:52 +0200

    edit home

--- src/app/common-components/product/form/form.component.ts
+++ src/app/common-components/product/form/form.component.ts
@@ -10,16 +10,19 @@ import {IProduct} from "../../../core/interfaces/product";
 export class FormComponent implements OnInit {
   title:string = 'Form';
   products: IProduct;
-  product : boolean | IProduct;
+  product : boolean | IProduct | object;
   constructor(private productsService: ProductsService) {
     this.product = productsService.isProdId$.value
-
+    // this.product = Object.values(productsService.isProdId$)
   }
 
   ngOnInit(): void {
     // this.productsService.getProductDetailsApi(20).subscribe((data) => {
     //   this.product = data;
+    this.product
+
       console.log(this.product)
+    console.log(typeof this.product)
     // });
   }
   // getafafadsf(): void {
